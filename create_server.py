cda03693332a5596e1795a95bfaee2e0d403bd87
Tushar Mahajan <tman08810@gmail.com>
2020-12-28 22:08:43 -0500

    changed source of data

--- create_server.py
+++ create_server.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python3
 
-from bs4 import BeautifulSoup
 import requests
+import json
 import os
 import shutil
 import subprocess
 import sys
 import math
 
-print("Minecraft Server Creator v1.4")
-print("This script will scrape the mcversion.net website and download a server")
+print("Minecraft Server Creator v2.0")
+print("This script will scrape the launchermeta.mojang.com website and download a server")
 print("It will then setup the server\n")
 
 # Create, Manage, Update
-url = "https://mcversions.net"
-mcversions = BeautifulSoup(requests.get(url).text, "html.parser")
-server_links = dict()
-version_categories = ["Stable Releases", "Snapshot Preview"]
-version_sections = list(mcversions.find_all("div", class_='versions')[0].children)
-for i in range(len(version_categories)):
-	version_items = version_sections[i].find_all(class_='item')
-	version_items = list(filter(lambda version_item: len(version_item['class']) == 1, version_items))
-	server_links[version_categories[i]] = dict(zip([version["id"] for version in version_items], [link.find("a", text="Download")["href"] for link in version_items]))
+response = json.loads(requests.get("https://launchermeta.mojang.com/mc/game/version_manifest.json").text)
+release = dict()
+snapshot = dict()
+for entry in response["versions"]:
+	if entry["type"] == "release":
+		release[entry["id"]] = entry["url"]
+	elif entry["type"] == "snapshot":
+		snapshot[entry["id"]] = entry["url"]
 
 version = input("Please enter a Minecraft version, \n" + 
 				"\"view\" to see all versions, \n" + 
@@ -30,41 +29,45 @@ version = input("Please enter a Minecraft version, \n" +
 
 while True:
 	if version == "release":
-		version = list(server_links[version_categories[0]].keys())[0]
-		category = version_categories[0]
+		version = response["latest"]["release"]
+		version_url = release[version]
 		break
 	if version == "snapshot":
-		version = list(server_links[version_categories[1]].keys())[0]
-		category = version_categories[1]
+		version = response["latest"]["snapshot"]
+		version_url = snapshot[version]
 		break
 	if version == "view":
-		columns = 4
-		for i in range(len(version_categories)):
-			print(version_categories[i] + ": ")
-			release = list(server_links[version_categories[i]].keys())
-			rows = math.ceil(len(release) / columns)
-			for i in range(rows):
-				print("\t", end="")
-				for j in range(columns):
-					if len(release) > i + j * rows:
-						print(release[i + j * rows], end="")
-						spacing = math.ceil(max((24 - len(release[i + j * rows])) / 8, 1)) * "\t"
-						print(spacing, end="")
-				print()	
-		version = input("Please enter a valid version number, \"release\" for the latest release version, or \"snapshot\" for the latest snapshot version: ")	
-		continue		
-	if version in server_links.get(version_categories[0]).keys():
-		category = version_categories[0]
+		print("Release:")
+		count = 0
+		for entry in release.keys():
+			if count < 4:
+				print(entry + "\t", end="")
+				count += 1
+			else:
+				print(entry)
+				count = 0
+		print("\n")
+		print("Snapshot:")
+		count = 0
+		for entry in snapshot.keys():
+			if count < 4:
+				print(entry + "\t\t", end="")
+				count += 1
+			else:
+				print(entry)
+				count = 0
+		print("\n")			
+	elif version in release.keys():
+		version_url = release[version]
 		break
-	if version in server_links.get(version_categories[1]).keys():
-		category = version_categories[1]
+	elif version in snapshot.keys():
+		version_url = snapshot[version]
 		break
 	version = input("Please enter a valid version number, \"release\" for the latest release version, or \"snapshot\" for the latest snapshot version: ")	
 
 print("Creating server with version: " + version)
 
-download = BeautifulSoup(requests.get(url + server_links[category][version]).text, "html.parser")
-mojang_url = download.find_all('a',class_="button")[0]['href']
+response = json.loads(requests.get(version_url).text)
 
 if getattr(sys, 'frozen', False):
     application_path = os.path.dirname(sys.executable)
@@ -77,7 +80,7 @@ if os.path.exists(server_directory):
 
 os.makedirs(server_directory)
 with open(server_directory + "/server.jar", "wb") as jar:
-    jar.write(requests.get(mojang_url).content)
+    jar.write(requests.get(response["downloads"]["server"]["url"]).content)
 
 eula = open(server_directory + "/eula.txt", "w")
 eula.write("eula=true")
